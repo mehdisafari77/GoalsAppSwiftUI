7636ff5c528777e1237ecf58834991caba5653ca
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-03-15 18:54:25 -0700

    Initial Commit

---
+++ GoalsAppSwiftUI/GoalsAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoalsAppSwiftUIApp.swift
+//  GoalsAppSwiftUI
+//
+//  Created by Mehdi MMS on 15/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct GoalsAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
