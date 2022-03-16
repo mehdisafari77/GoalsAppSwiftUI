b8056421c305741f1798c2feec4d23bcc51cf7e2
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-03-15 23:15:57 -0700

    emoji textfield extension

---
+++ GoalsAppSwiftUI/Utils/EmojiTextField.swift
@@ -0,0 +1,71 @@
+//
+//  EmojiTextField.swift
+//  GoalsAppSwiftUI
+//
+//  Created by Mehdi MMS on 15/03/2022.
+//
+
+import SwiftUI
+import UIKit
+
+class UIEmojiTextField: UITextField {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    func setEmoji() {
+        _ = self.textInputMode
+    }
+    
+    override var textInputContextIdentifier: String? {
+        return ""
+    }
+    
+    override var textInputMode: UITextInputMode? {
+        for mode in UITextInputMode.activeInputModes {
+            if mode.primaryLanguage == "emoji" {
+                self.keyboardType = .default
+                return mode
+            }
+        }
+        
+        return nil
+    }
+}
+
+struct EmojiTextField: UIViewRepresentable {
+    @Binding var text: String
+    var placeholder: String = ""
+    
+    func makeUIView(context: Context) -> UIEmojiTextField {
+        let txtField = UIEmojiTextField()
+        txtField.placeholder = placeholder
+        txtField.text = text
+        txtField.delegate = context.coordinator
+        return txtField
+    }
+    
+    func updateUIView(_ uiView: UIEmojiTextField, context: Context) {
+        uiView.text = text
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(parent: self)
+    }
+    
+    class Coordinator: NSObject, UITextFieldDelegate {
+        var parent: EmojiTextField
+        
+        init(parent: EmojiTextField) {
+            self.parent = parent
+        }
+        
+        func textFieldDidChangeSelection(_ textField: UITextField) {
+            DispatchQueue.main.async { [weak self] in
+                self?.parent.text = textField.text ?? ""
+            }
+        }
+    }
+    
+}
