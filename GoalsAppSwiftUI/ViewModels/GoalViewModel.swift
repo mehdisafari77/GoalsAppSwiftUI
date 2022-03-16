f16f57bd88f95f77855d292e6679a3e245a578b7
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-03-15 23:20:01 -0700

    finish the GoalVM

---
+++ GoalsAppSwiftUI/ViewModels/GoalViewModel.swift
@@ -0,0 +1,48 @@
+//
+//  GoalViewModel.swift
+//  GoalsAppSwiftUI
+//
+//  Created by Mehdi MMS on 15/03/2022.
+//
+
+import Foundation
+import UIKit
+import SwiftUI
+
+struct GoalViewModel {
+    var goal: Goal
+    
+    var id: String {
+        goal.id ?? ""
+    }
+    
+    var name: String {
+        goal.name
+    }
+    
+    var dueOnDate: Date {
+        goal.dueOn
+    }
+    
+    var dueOn: String {
+        goal.dueOn.toRelativeDate()
+    }
+    
+    var color: Color {
+        Color(UIColor(hexString: goal.color))
+    }
+    
+    var icon: String {
+        goal.icon
+    }
+    
+    var items: [String] {
+        get {
+            return goal.items
+        }
+        
+        set(newItems) {
+            goal.items = newItems
+        }
+    }
+}
