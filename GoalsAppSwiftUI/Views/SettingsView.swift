f73f33644fc1ffd1126b6044c95c16ec1c733895
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-03-15 23:43:43 -0700

    add a settings view

---
+++ GoalsAppSwiftUI/Views/SettingsView.swift
@@ -0,0 +1,98 @@
+//
+//  SettingsView.swift
+//  GoalsAppSwiftUI
+//
+//  Created by Mehdi MMS on 15/03/2022.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    
+    let defaultURL = URL(string: "https://www.google.com")!
+    let coingeckoURL = URL(string: "https://www.coingecko.com")!
+    let personalURL = URL(string: "https://mehdisafari77.github.io/React-Portfolio/")!
+    let githubURL = URL(string: "https://github.com/mehdisafari77")!
+
+    var body: some View {
+        NavigationView {
+            ZStack {
+                // background
+                Color.theme.background
+                    .ignoresSafeArea()
+                // content
+                List {
+                    coinGeckoSection
+                        .listRowBackground(Color.theme.background.opacity(0.5))
+                    developerSection
+                        .listRowBackground(Color.theme.background.opacity(0.5))
+                    applicationSection
+                        .listRowBackground(Color.theme.background.opacity(0.5))
+                }
+            }
+            .font(.headline)
+            .accentColor(.blue)
+            .listStyle(GroupedListStyle())
+            .navigationTitle("Settings")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    XMarkButton()
+                }
+            }
+        }
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
+extension SettingsView {
+    
+    private var coinGeckoSection: some View {
+        Section(header: Text("Coin Gecko")) {
+            VStack(alignment: .leading) {
+                Image("coingecko")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 100)
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                Text("The cryptocurrency data that is used in this app comes from a free API from CoinGecko! Prices may be slightly delayed.")
+                    .font(.callout)
+                    .fontWeight(.medium)
+                    .foregroundColor(Color.theme.accent)
+            }
+            .padding(.vertical)
+            Link("Visit CoinGecko 🥳", destination: coingeckoURL)
+        }
+    }
+    
+    private var developerSection: some View {
+        Section(header: Text("Developer")) {
+            VStack(alignment: .leading) {
+                Image("logo")
+                    .resizable()
+                    .frame(width: 100, height: 100)
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                Text("This app was developed by Mehdi Safari. It uses SwiftUI and is written 100% in Swift. The project benifits from multi-threading, publishers/subscribers and data persistence.")
+                    .font(.callout)
+                    .fontWeight(.medium)
+                    .foregroundColor(Color.theme.accent)
+            }
+            .padding(.vertical)
+            Link("Visit Github Repo 🔥", destination: githubURL)
+            Link("Visit Website 🥳", destination: personalURL)
+        }
+    }
+
+    private var applicationSection: some View {
+        Section(header: Text("Application")) {
+            Link("Terms of Service", destination: defaultURL)
+            Link("Privacy Policy", destination: defaultURL)
+            Link("Company Website", destination: defaultURL)
+            Link("Learn More", destination: defaultURL)
+        }
+    }
+
+}
