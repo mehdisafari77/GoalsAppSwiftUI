919cc3691e53b7401af14f2dfa702a1a6097f4d3
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-03-15 23:18:38 -0700

    add animation extension

--- GoalsAppSwiftUI/Utils/AnimationUtil.swift
+++ GoalsAppSwiftUI/Utils/AnimationUtil.swift
@@ -5,4 +5,14 @@
 //  Created by Mehdi MMS on 15/03/2022.
 //
 
-import Foundation
+import SwiftUI
+
+extension Animation {
+    func `repeat`(while expression: Bool, autoreverses: Bool = true) -> Animation {
+        if expression {
+            return self.repeatForever(autoreverses:  autoreverses)
+        } else {
+            return self
+        }
+    }
+}
